eb6f61bae061142a2b357a5cf6859f41accef9c8
Rafaela Pedroso <rafaela.vp@outlook.com>
2022-12-23 12:33:20 -0300

    Adicionando itens na lista

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,11 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+// console.log(listaDeAlunosEMedias);
+
+//                                              Pega 1ª lista e o 2º Nome
+console.log(`A Aluna da posição 1 da lista é: ${listaDeAlunosEMedias[0][1]}
+A nota dessa aluna é ${listaDeAlunosEMedias[1][1]
+}`);
